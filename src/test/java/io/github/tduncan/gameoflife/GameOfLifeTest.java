f6ac6765a93936fc545496612b1edf5dfc806f4c
thomasduncan <=>
2020-04-30 22:51:08 -0700

    Before starting to code we want to first think about the probllem we are solving. This step, while not explicitly named in the red-green-refactor workflow, is a vital step in the process.  We aren't trying to find answers here, but use cases.  Also, be careful not to go overboard here. Our goal during this time isn't to discover the complete specification, but to ennumerate the initially obvious use cases with the expectation that we will think of or discover additional use cases as we flesh out the tests and the solution.

---
+++ src/test/java/io/github/tduncan/gameoflife/GameOfLifeTest.java
@@ -0,0 +1,15 @@
+package io.github.tduncan.gameoflife;
+
+class GameOfLifeTest {
+    // all cells are dead, should all remain dead
+    // all cells are alive
+    // live cell with zero live neighbors should die
+    // live cell with one live neighbor, should die
+    // live cell with two live neighbors, should live
+    // live cell with three live neighbors, should live
+    // live cell with four live neighbors, should die
+    // dead cell with one live neighbor, should remain dead
+    // deal cell with two live neighbors, should remain dead
+    // dead cell with three live neighbors, should be alive
+    // dead cell with four live neighbors, should be dead
+}
