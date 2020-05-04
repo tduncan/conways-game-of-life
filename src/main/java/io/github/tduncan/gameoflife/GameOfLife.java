72dcc8c8dcbaeef0fafc031ca4911cea6cafc2b9
thomasduncan <=>
2020-05-03 23:03:32 -0700

    Taking another look at our list of use cases, the next one up is 'live cell with zero live neighbors should die', which seems like a reasonable place to go next. To keep things skmple we are going to use 3x3 grids, with the center cell as our 'pivot' point.
    org.opentest4j.AssertionFailedError: array contents differ at index [1][1], expected: <0> but was: <1>
    	at io.github.tduncan.gameoflife.GameOfLifeTest.gridWithOnlySingleLiveCellShouldDieOutEntirely(GameOfLifeTest.java:66)

--- src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
+++ src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
@@ -2,9 +2,6 @@ package io.github.tduncan.gameoflife;
 
 class GameOfLife {
     int[][] nextGeneration(int[][] grid) {
-        // process all cells
-        // compute when cells die
-        // compute when cells continue to live
         return grid;
     }
 }
--- src/test/java/io/github/tduncan/gameoflife/GameOfLifeTest.java
+++ src/test/java/io/github/tduncan/gameoflife/GameOfLifeTest.java
@@ -52,6 +52,34 @@ class GameOfLifeTest {
     }
 
     @Test
+    void gridWithOnlySingleLiveCellShouldDieOutEntirely() {
+        var grid = new int[][] {
+                {0,0,0},
+                {0,1,0},
+                {0,0,0}
+        };
+
+        var game = new GameOfLife();
+        var nextGeneration = game.nextGeneration(grid);
+
+        var allDead = new int[][]{
+                {0,0,0},
+                {0,0,0},
+                {0,0,0}
+        };
+        assertArrayEquals(allDead, nextGeneration);
+    }
+
+    // live cell with one live neighbor, should die
+    // live cell with two live neighbors, should live
+    // live cell with three live neighbors, should live
+    // live cell with four live neighbors, should die
+    // dead cell with one live neighbor, should remain dead
+    // deal cell with two live neighbors, should remain dead
+    // dead cell with three live neighbors, should be alive
+    // dead cell with four live neighbors, should be dead
+
+    @Test
     @Disabled
     void gridContainAllLiveCellsWillOnlyHaveCornersRemainingAlive() {
         var allAlive = new int[][]{
@@ -70,15 +98,4 @@ class GameOfLifeTest {
         };
         assertArrayEquals(onlyCornersAlive, nextGeneration);
     }
-
-    // all cells are alive
-    // live cell with zero live neighbors should die
-    // live cell with one live neighbor, should die
-    // live cell with two live neighbors, should live
-    // live cell with three live neighbors, should live
-    // live cell with four live neighbors, should die
-    // dead cell with one live neighbor, should remain dead
-    // deal cell with two live neighbors, should remain dead
-    // dead cell with three live neighbors, should be alive
-    // dead cell with four live neighbors, should be dead
 }
