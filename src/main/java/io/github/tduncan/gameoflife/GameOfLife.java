46070e35de6e4f7a3081df19b783f207304105aa
thomasduncan <=>
2020-05-05 23:02:51 -0700

    We are able to pass the test, but this should again make you feel uncomfortable. Why the are we concerned with the middle cell? The answer, of course, is because our tests haven't asked us to care about anything else. We have made some progress, and I don't feel the need to refactor just yet, so let's devise a test that will force the solution away from only modifying the center cell.

--- src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
+++ src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
@@ -2,6 +2,9 @@ package io.github.tduncan.gameoflife;
 
 class GameOfLife {
     int[][] nextGeneration(int[][] grid) {
+        int xMiddle = grid.length / 2;
+        int yMiddle = grid[0].length / 2;
+        grid[xMiddle][yMiddle] = 0;
         return grid;
     }
 }
