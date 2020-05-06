09d5f6cde972dad8408b35b1975aeebe2f6b5fa6
thomasduncan <=>
2020-05-05 23:41:14 -0700

    Passed! It's not a lot of code, but what is here is tricky.  This is our first real opportunity to improve the solution in the refactoring phase so lets take this opportunity.

--- src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
+++ src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
@@ -4,7 +4,23 @@ class GameOfLife {
     int[][] nextGeneration(int[][] grid) {
         for(int x = 0; x < grid.length; x++) {
             for(int y = 0; y < grid[x].length; y++) {
-                grid[x][y] = 0;
+
+                int aliveNeighbors = 0;
+                // Pivot around the cell at x,y, keeping track of how many live neighboring cells are found
+                for(int nx = x - 1; nx < x + 2; nx++) {
+                    for(int ny = y - 1; ny < y + 2; ny++) {
+                        if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[x].length) {
+                            if(nx == x && ny == y) {  // Don't count the pivot point
+                                continue;
+                            }
+                            aliveNeighbors += grid[nx][ny];
+                        }
+                    }
+                }
+
+                if(aliveNeighbors != 2) {
+                    grid[x][y] = 0;
+                }
             }
         }
         return grid;
