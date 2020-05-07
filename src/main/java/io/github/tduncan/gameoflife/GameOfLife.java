ef6e4f330402df5a3c1220a0f212e68822acdc4a
thomasduncan <=>
2020-05-06 23:34:31 -0700

    Before we start refactoring lets take a moment to consider what makes the existing code difficult to understand. You probably immediately thought of the variables 'nx' and 'ny', which aren't great. There is also the problem of the 4 nested loops, at the bottom of which mutates a temporary variable. All of this is bad, but the reason for most of this is that our method is trying to do two things at: 1. count the number of alive neighbors and 2. change the next generation state based of that count.
    So, rather than refactors at the smallest scale, we are actually going to first extract the alive neighbor counting logic into a dedicated method. We'll name this method something obvious like 'countAliveNeighbors' and provide all of the information necessary for it to accomplish the task.
    
    Doing this yields immediate results. While we still have hte temporary
    variable 'aliveNeighbors', we are no longer changing it within our outer
    double for loop. This simple change is significantly easier to
    understand, and significantly less prone to error.
    
    Our new method 'countAliveNeighbors' is also very simple. The bad names
    still exist, but there are zero side effects and it does exactly what
    its name claims, a great trait for any bit of code!

--- src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
+++ src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
@@ -4,26 +4,29 @@ class GameOfLife {
     int[][] nextGeneration(int[][] grid) {
         for(int x = 0; x < grid.length; x++) {
             for(int y = 0; y < grid[x].length; y++) {
-
-                int aliveNeighbors = 0;
-                // Pivot around the cell at x,y, keeping track of how many live neighboring cells are found
-                for(int nx = x - 1; nx < x + 2; nx++) {
-                    for(int ny = y - 1; ny < y + 2; ny++) {
-                        if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[x].length) {
-                            if(nx == x && ny == y) {  // Don't count the pivot point
-                                continue;
-                            }
-                            aliveNeighbors += grid[nx][ny];
-                        }
-                    }
-                }
-
+                int aliveNeighbors = countAliveNeighbors(grid, x, y);
                 if(aliveNeighbors != 2) {
                     grid[x][y] = 0;
                 }
             }
         }
         return grid;
+    }
+
+    private int countAliveNeighbors(int[][] grid, int x, int y) {
+        int aliveNeighbors = 0;
+        // Pivot around the cell at x,y, keeping track of how many live neighboring cells are found
+        for(int nx = x - 1; nx < x + 2; nx++) {
+            for(int ny = y - 1; ny < y + 2; ny++) {
+                if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[x].length) {
+                    if(nx == x && ny == y) {  // Don't count the pivot point
+                        continue;
+                    }
+                    aliveNeighbors += grid[nx][ny];
+                }
+            }
+        }
+        return aliveNeighbors;
     }
 }
 
