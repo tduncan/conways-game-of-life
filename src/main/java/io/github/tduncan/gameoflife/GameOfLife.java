141f396bacc214287c64d22d0284cbc19d3c881b
thomasduncan <=>
2020-05-08 00:03:44 -0700

    Excellent, we're back to green! Now lets revisit our disabled tests from earlier.

--- src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
+++ src/main/java/io/github/tduncan/gameoflife/GameOfLife.java
@@ -6,7 +6,7 @@ class GameOfLife {
         for(int x = 0; x < grid.length; x++) {
             for(int y = 0; y < grid[x].length; y++) {
                 int aliveNeighbors = countAliveNeighbors(grid, x, y);
-                if (aliveNeighbors != 2) {
+                if (aliveNeighbors != 2 && aliveNeighbors != 3) {
                     nextGeneration[x][y] = 0;
                 } else {
                     nextGeneration[x][y] = grid[x][y];
