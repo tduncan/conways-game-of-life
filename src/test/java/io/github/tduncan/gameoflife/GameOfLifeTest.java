ef622177e054c04ebdb4573d6a1fef90e51e85de
thomasduncan <=>
2020-05-07 23:22:35 -0700

    And the test passes... oops! No worries, however. While ideally all of our tests will fail at first, this is a normal situation to find yourself in so don't worry.
    When this happens, take a step back to understand why? And to consider more deeply what scenario will can be converted into a failing test. If you can't think of one, congrats! You may be done! Otherwise, start writing that new test. For us, that test will be a scenario that causes a dead cell to be alive in the next generation. In our notes we jotted down "dead cell with three live neighbors, should be alive", so we'll start there.

--- src/test/java/io/github/tduncan/gameoflife/GameOfLifeTest.java
+++ src/test/java/io/github/tduncan/gameoflife/GameOfLifeTest.java
@@ -108,7 +108,25 @@ class GameOfLifeTest {
         assertArrayEquals(expectedNextGeneration, nextGeneration);
     }
 
-    // live cell with one live neighbor, should die
+    @Test
+    void liveCellWithSingleLiveNeighborsDiesInNextGeneration() {
+        var grid = new int[][] {
+                {1,1,0},
+                {0,0,0},
+                {0,0,0}
+        };
+
+        var game = new GameOfLife();
+        var nextGeneration = game.nextGeneration(grid);
+
+        var expectedNextGeneration = new int[][] {
+                {0,0,0},
+                {0,0,0},
+                {0,0,0}
+        };
+        assertArrayEquals(expectedNextGeneration, nextGeneration);
+    }
+
     // live cell with three live neighbors, should live
     // live cell with four live neighbors, should die
     // dead cell with one live neighbor, should remain dead
